48d6100e0d512044b150b21d1af4cdb21ebc5fa1
Bartosz <barzak200@gmail.com>
2021-08-21 13:14:42 +0200

    Finishing shades of blue color scheme

--- js/changing-style.js
+++ js/changing-style.js
@@ -15,7 +15,7 @@ function addListeners() {
 
   buttonEls[3].addEventListener('click', () => {
     changeTo(turquoise);
-    addShades(blueShade1, blueShade2);
+    addShades(blueShade1, blueShade2, blueShade3);
   });
 }
 
@@ -25,20 +25,25 @@ const turquoise = '#01627f';
 
 const blueShade1 = '#003c57';
 const blueShade2 = '#5ba1c1';
+const blueShade3 = '#8fcbe6';
 
 function changeTo(color) {
   headingEl.style.color = color;
+
   logoEl.style.color = color;
 
   aboutBoxEl.style.borderColor = color;
 
   ctaEl.style.backgroundColor = color;
+
   openingHoursEl.style.backgroundColor = color;
+
   submitBtnEl.style.backgroundColor = color;
+
   footerEl.style.backgroundColor = color;
 }
 
-function addShades(color, color2) {
+function addShades(color, color2, color3) {
   footerDescEl.style.backgroundColor = color;
 
   contactLabelsEl.forEach((contactLabel) => {
@@ -48,6 +53,28 @@ function addShades(color, color2) {
   contactEmailEl.style.color = color2;
 
   ctaButtonEl.style.backgroundColor = color2;
+
+  headersAboutEl.forEach((headerAbout) => {
+    headerAbout.style.color = color2;
+  });
+
+  openingHoursEl.style.backgroundColor = color;
+
+  comeForVisitHeadingEl.style.color = color3;
+
+  daysEl.forEach((day) => {
+    day.style.color = color3;
+  });
+
+  ctaEl.style.backgroundColor = color;
+
+  headerCtaEl.style.color = color3;
+
+  footerDescEl.style.color = color3;
+
+  footerIconsEl.forEach((footerIcon) => {
+    footerIcon.style.color = color3;
+  });
 }
 
 addListeners();
@@ -64,3 +91,8 @@ const footerDescEl = document.querySelector('.footer-description');
 const contactLabelsEl = document.querySelectorAll('.contact-label');
 const contactEmailEl = document.querySelector('.contact-link--email');
 const ctaButtonEl = document.querySelector('.cta-btn');
+const headersAboutEl = document.querySelectorAll('.header-about');
+const comeForVisitHeadingEl = document.querySelector('.come-for-a-visit-heading');
+const daysEl = document.querySelectorAll('.day');
+const headerCtaEl = document.querySelector('.header-cta');
+const footerIconsEl = document.querySelectorAll('.footer-icon');
