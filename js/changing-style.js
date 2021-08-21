29db3c3fdfe61e7d8c66cccac24184ffa97e3913
Bartosz <barzak200@gmail.com>
2021-08-21 12:16:24 +0200

    Add fist main color - brown

--- js/changing-style.js
+++ js/changing-style.js
@@ -1,9 +1,31 @@
 function addListeners() {
   const buttonEl = document.querySelectorAll('.change-style');
   buttonEl[0].addEventListener('click', () => {
-    const headingEl = document.querySelector('.heading-primary');
-    headingEl.style.color = 'blue';
+    changeTo(brown);
   });
+}
+
+const headingEl = document.querySelector('.heading-primary');
+// const navEl = document.querySelector('.main-nav-list');
+const logoEl = document.querySelector('.logo');
+const aboutBoxEl = document.querySelector('.about-text-box');
+const ctaEl = document.querySelector('.cta');
+const openingHoursEl = document.querySelector('.opening-hours');
+const submitBtnEl = document.querySelector('.btn-submit');
+const footerEl = document.querySelector('.footer');
+
+const brown = '#9f6243';
+
+function changeTo(color) {
+  headingEl.style.color = color;
+  logoEl.style.color = color;
+
+  aboutBoxEl.style.borderColor = color;
+
+  ctaEl.style.backgroundColor = color;
+  openingHoursEl.style.backgroundColor = color;
+  submitBtnEl.style.backgroundColor = color;
+  footerEl.style.backgroundColor = color;
 }
 
 addListeners();
