ff8da33d796dbb151a9c28d98015fbbf99f98f76
Bartosz <barzak200@gmail.com>
2021-08-21 12:42:52 +0200

    Add github logo

--- js/changing-style.js
+++ js/changing-style.js
@@ -1,23 +1,20 @@
 function addListeners() {
   const buttonEls = document.querySelectorAll('.change-style');
+
   buttonEls[0].addEventListener('click', () => {
-    changeTo(brown);
+    changeTo(black);
   });
 
   buttonEls[1].addEventListener('click', () => {
+    changeTo(brown);
+  });
+
+  buttonEls[2].addEventListener('click', () => {
     changeTo(turquoise);
   });
 }
 
-const headingEl = document.querySelector('.heading-primary');
-// const navEl = document.querySelector('.main-nav-list');
-const logoEl = document.querySelector('.logo');
-const aboutBoxEl = document.querySelector('.about-text-box');
-const ctaEl = document.querySelector('.cta');
-const openingHoursEl = document.querySelector('.opening-hours');
-const submitBtnEl = document.querySelector('.btn-submit');
-const footerEl = document.querySelector('.footer');
-
+const black = '#000';
 const brown = '#9f6243';
 const turquoise = '#01627f';
 
@@ -34,3 +31,12 @@ function changeTo(color) {
 }
 
 addListeners();
+
+const headingEl = document.querySelector('.heading-primary');
+// const navEl = document.querySelector('.main-nav-list');
+const logoEl = document.querySelector('.logo');
+const aboutBoxEl = document.querySelector('.about-text-box');
+const ctaEl = document.querySelector('.cta');
+const openingHoursEl = document.querySelector('.opening-hours');
+const submitBtnEl = document.querySelector('.btn-submit');
+const footerEl = document.querySelector('.footer');
